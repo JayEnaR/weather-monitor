058e80d22099f3edf5cc0932454c90a548c8e87f
ervin <ervinvdmerwe@yahoo.com>
2024-11-08 14:13:46 +0200

    Install chart lib

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { MatMenu } from '@angular/material/menu';
 import { MatIcon } from '@angular/material/icon';
+import { ApexOptions, NgApexchartsModule } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatMenu, MatIcon],
+  imports: [MatMenu, MatIcon, NgApexchartsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  public chartOptions: ApexOptions;
 
+  constructor(){
+    this.chartOptions = {
+      
+    }
+  }
 }
