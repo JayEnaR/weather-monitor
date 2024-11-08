f10ef0bf30f86ad19a98dc8a6be947d365d6decd
ervin <ervinvdmerwe@yahoo.com>
2024-11-08 13:53:11 +0200

    Setup basic dashboard elements

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatMenu } from '@angular/material/menu';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatMenu, MatIcon],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
