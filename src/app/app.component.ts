67f94c589482814c7174fb2c1caff895e8fc7d30
JayEnaR <ervinvdmerwe@yahoo.com>
2024-11-09 15:43:57 +0200

    Top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIcon, MatIconRegistry } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { ClientStatusService } from './services/client-status.service';
+import { Subject, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton, MatIcon, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'weather-monitor';
+export class AppComponent  {
+
+  online: boolean = false;
+
+  constructor(private _matIconReg: MatIconRegistry,
+    public _clientStatusService: ClientStatusService) {
+
+    this._matIconReg.setDefaultFontSetClass('material-symbols-outlined');
+
+    this._clientStatusService.status$.subscribe(res => this.online = res);
+  }
+
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -31,8 +31,9 @@ export class DashboardComponent {
   ) {
     this.initMqtt();
     this.chartOptions = {};
-    
+
     this._mqttService.onConnect.subscribe(res => console.log(res));
+    this._mqttService.onMessage.subscribe(msg => console.log(msg));
     this._mqttService.onOffline.subscribe(() => {
       // Update client status
       this._clientStatusService.updateStatus(false);
@@ -45,9 +46,10 @@ export class DashboardComponent {
       .observeRetained(MQTT_TOPCIS.connectionStatus, { qos: 1, rap: false })
       .subscribe((m: IMqttMessage) => {
         console.log(m);
-        
+
         const statusResponse = m.payload.toString();
-        const isOnline: boolean = statusResponse === 'Online' ? true : false;
+        const isOnline: boolean = (statusResponse.trim().toLowerCase().startsWith('connect') ? true : false);
+        console.log(`Online: ${isOnline} (${statusResponse})`);
 
         if (isOnline) {
           // Temperature
