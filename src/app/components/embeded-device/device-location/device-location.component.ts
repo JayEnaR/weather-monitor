b7774565ab06740238783ac7c3ba7e5440096d3b
ervin <ervinvdmerwe@yahoo.com>
2024-11-20 16:00:14 +0200

    Find device

--- src/app/components/embeded-device/device-location/device-location.component.ts
+++ src/app/components/embeded-device/device-location/device-location.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { MqttService } from 'ngx-mqtt';
 import { MQTT_TOPCIS } from '../../../mqtt/mqtt_topics';
 import { MatButtonModule } from '@angular/material/button';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-device-location',
@@ -10,28 +11,61 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './device-location.component.html',
   styleUrl: './device-location.component.scss',
 })
-export class DeviceLocationComponent {
-  coordinates: string = '';
+export class DeviceLocationComponent implements AfterViewInit {
+  coordinates: L.LatLngLiteral;
+  private map: any;
 
   constructor(private _mqttService: MqttService) {
+    this.coordinates = {} as L.LatLngLiteral;
     this.initSubscriptions();
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+
+  initMap(): void {
+    this.map = L.map('map', {
+      center: this.coordinates,
+      zoom: 18,
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: ''
+    });
+
+    tiles.addTo(this.map);
+    var circle = L.circle([-26.69561, 27.07717], {
+      color: 'red',
+      fillColor: '#ff003378',
+      fillOpacity: 0.5,
+      radius: 40
+  }).addTo(this.map);
+      
   }
 
   initSubscriptions(): void {
     this._mqttService
       .observe(MQTT_TOPCIS.coordinates, { qos: 1, rap: true })
       .subscribe((res) => {
-        const coord = res.payload.toString();
-        this.coordinates = coord;
+        const coord = res.payload.toString().split(',')
+
+        this.coordinates.lat = +coord[0];
+        this.coordinates.lng = +coord[1];
         /**
          * TODO:
          * Use coordinates to display in map
          * Update map when coordinates changes
          */
+        this.initMap();
       });
   }
 
   findDevice(): void {
+    // this.map.setView([-26.69585, 27.07785], 18 );
+
+    // TODO: Temp and humid go to zero after find device is called
     this._mqttService
       .publish(MQTT_TOPCIS.findDevice, 'find_device', { qos: 1, retain: false })
       .subscribe(() => {
