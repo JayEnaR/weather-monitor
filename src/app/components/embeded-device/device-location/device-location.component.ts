35eca987133ea7ceab9d471cd5cd961d58066f34
ervin <ervinvdmerwe@yahoo.com>
2024-11-21 15:20:23 +0200

    Move online status check to component

--- src/app/components/embeded-device/device-location/device-location.component.ts
+++ src/app/components/embeded-device/device-location/device-location.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { MqttService } from 'ngx-mqtt';
 import { MQTT_TOPCIS } from '../../../mqtt/mqtt_topics';
 import { MatButtonModule } from '@angular/material/button';
 import * as L from 'leaflet';
 import { ClientStatusService } from '../../../services/client-status.service';
 import { CommonModule } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-device-location',
@@ -13,14 +14,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './device-location.component.html',
   styleUrl: './device-location.component.scss',
 })
-export class DeviceLocationComponent implements AfterViewInit {
+export class DeviceLocationComponent implements AfterViewInit, OnDestroy {
   coordinates: L.LatLngLiteral;
   private map: any;
   circle: any;
+  isOnline: boolean = false;
+  unsub$: Subject<void> = new Subject<void>();
 
   constructor(private _mqttService: MqttService, public _clientStatusService: ClientStatusService) {
     this.coordinates = { lat: 0, lng: 0 };
     this.initSubscriptions();
+    this._clientStatusService.status$.pipe(takeUntil(this.unsub$)).subscribe(stat => this.isOnline = stat);
   }
 
   ngAfterViewInit(): void {
@@ -53,8 +57,9 @@ export class DeviceLocationComponent implements AfterViewInit {
 
   initSubscriptions(): void {
     this._mqttService
-      .observe(MQTT_TOPCIS.coordinates, { qos: 1, rap: true })
+      .observeRetained(MQTT_TOPCIS.coordinates, { qos: 1 })
       .subscribe((res) => {
+
         const coord = res.payload.toString().split(',');
 
         this.coordinates.lat = +coord[0];
@@ -75,7 +80,11 @@ export class DeviceLocationComponent implements AfterViewInit {
   }
 
   hasCoordinates(): boolean {
-    debugger
     return this.coordinates.lat != 0 && this.coordinates.lng != 0;
+  }
+
+  ngOnDestroy(): void {
+    this.unsub$.next();
+    this.unsub$.complete();
   }
 }
