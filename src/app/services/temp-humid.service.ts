1080fed964163202c28fc28925c446d84f655ecd
ervin <ervinvdmerwe@yahoo.com>
2024-12-16 21:00:54 +0200

    Temp humid service save values to indexed db

--- src/app/services/temp-humid.service.ts
+++ src/app/services/temp-humid.service.ts
@@ -5,6 +5,8 @@ import { MqttService } from 'ngx-mqtt';
 import { ClientStatusService } from './client-status.service';
 import { MQTT_TOPCIS } from '../mqtt/mqtt_topics';
 import { Time } from '../helpers/time';
+import { IndexedDbService } from './indexed-db.service';
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +14,15 @@ import { Time } from '../helpers/time';
 export class TempHumidService {
   private tempHumidSrc = new ReplaySubject<ITempHumidModel>(1);
   $tempHumid = this.tempHumidSrc.asObservable();
+  intervals: number;
 
   constructor(
     private _mqttService: MqttService,
-    private _clientStatusService: ClientStatusService
+    private _clientStatusService: ClientStatusService,
+    private _indexedDbService: IndexedDbService,
+    private _configService: ConfigService
   ) {
+    this.intervals = this._configService.config.chartSeriesIntervals;
     this.observeTempHumid();
   }
 
@@ -27,11 +33,21 @@ export class TempHumidService {
           this._mqttService.observe(MQTT_TOPCIS.temperature, { qos: 1 }),
           this._mqttService.observe(MQTT_TOPCIS.humidity, { qos: 1 }),
         ]).subscribe(([temp, humid]) => {
-          this.tempHumidSrc.next({
+          const obj: ITempHumidModel = {
             id: (temp.messageId! + humid.messageId!).toString(),
             temperature: +temp.payload,
             humidity: +humid.payload,
             time: Time.getTime(),
+          };
+          this.tempHumidSrc.next(obj);
+          // State management
+          this._indexedDbService.getRowCount().subscribe((c) => {
+            // Only hold x amount of items
+            if (c == this.intervals) {
+              // Delete oldest
+              this._indexedDbService.removeFirst();
+            }
+            this._indexedDbService.add(obj);
           });
         });
       }
