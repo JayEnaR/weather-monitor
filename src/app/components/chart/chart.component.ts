f3d04449e207633577ec89f581fc7ef19bdec4bb
ervin <ervinvdmerwe@yahoo.com>
2024-12-17 22:45:42 +0200

    Modify chart to get temp and humid from service

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -8,6 +8,7 @@ import {
 import { ConfigService } from '../../services/config.service';
 import { IndexedDbService } from '../../services/indexed-db.service';
 import { Time } from '../../helpers/time';
+import { TempHumidService } from '../../services/temp-humid.service';
 
 @Component({
   selector: 'app-chart',
@@ -16,10 +17,7 @@ import { Time } from '../../helpers/time';
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.scss',
 })
-export class ChartComponent implements OnChanges {
-  @Input() temperature: number = 0;
-  @Input() humidity: number = 0;
-
+export class ChartComponent {
   private intervals: number;
   private tempArr: number[] = [];
   private humidArr: number[] = [];
@@ -29,7 +27,8 @@ export class ChartComponent implements OnChanges {
 
   constructor(
     private _configService: ConfigService,
-    private _indexeDbService: IndexedDbService
+    private _indexeDbService: IndexedDbService,
+    private _tempHumidService: TempHumidService
   ) {
     this.intervals = this._configService.config.chartSeriesIntervals;
 
@@ -51,8 +50,9 @@ export class ChartComponent implements OnChanges {
       colors: ['#3b82f6', '#f59e0b'],
     };
 
+    // State management
     this._indexeDbService.getAllItems().subscribe((res) => {
-      // Translate this array to the 2 arrays that display temp and humid
+      // Split the res array into temp and hmid
       res.forEach((i) => {
         this.humidArr.push(i.humidity), this.tempArr.push(i.temperature);
       });
@@ -61,34 +61,27 @@ export class ChartComponent implements OnChanges {
         { data: this.humidArr, name: 'Humidity' },
       ];
     });
-  }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    const temp = changes['temperature'];
-    const humid = changes['humidity'];
-    if (temp || humid) {
-      this.updateChart(temp?.currentValue, humid?.currentValue);
-    }
+    // Temp and Humid Subscription
+    this._tempHumidService.$tempHumid.subscribe((res) => {
+      this.updateChart(res.temperature, res.humidity);
+    });
   }
 
   updateChart(temp?: number, humid?: number): void {
-    // const time = Time.getTime();
-    //   this.seriesArr.push(time);
-    //   this.tempArr.push(
-    //     temp ?? this.tempArr[this.humidArr.length - 1]
-    //   );
-    //   this.humidArr.push(
-    //     humid ?? this.humidArr[this.humidArr.length - 1]
-    //   );
-    //   // We only want to see x intervals
-    //   if (this.tempArr.length == this.intervals + 1) {
-    //     this.tempArr.shift();
-    //     this.humidArr.shift();
-    //   }
-    //   this.chartOptions.series = [
-    //     { data: this.tempArr, name: 'Temperature' },
-    //     { data: this.humidArr, name: 'Humidity' },
-    //   ];
-    //   this.chartOptions.xaxis!.categories = this.seriesArr;
+    const time = Time.getTime();
+    this.seriesArr.push(time);
+    this.tempArr.push(temp ?? this.tempArr[this.humidArr.length - 1]);
+    this.humidArr.push(humid ?? this.humidArr[this.humidArr.length - 1]);
+    // We only want to see x intervals
+    if (this.tempArr.length == this.intervals + 1) {
+      this.tempArr.shift();
+      this.humidArr.shift();
+    }
+    this.chartOptions.series = [
+      { data: this.tempArr, name: 'Temperature' },
+      { data: this.humidArr, name: 'Humidity' },
+    ];
+    this.chartOptions.xaxis!.categories = this.seriesArr;
   }
 }
